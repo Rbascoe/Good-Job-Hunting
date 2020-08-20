014b3562f49f0408cdd1092b3fd91612cacfa9ff
JinaStrickland <Jina30@gmail.com>
2020-08-20 18:03:46 -0400

    added little bit of css

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,4 +2,9 @@ class Job < ApplicationRecord
     has_many :auditions
     has_many :users_auditions, through: :auditions, source: :users
     belongs_to :user
+
+    def display_title
+        return self.title.titlecase 
+    end
+    
 end
